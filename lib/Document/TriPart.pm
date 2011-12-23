b5472ff420f36e84b1ef7280a66a08041db7419f
robertkrimen <robertkrimen@gmail.com>
2011-12-23 14:35:34 -0800

    Convert to Dist::Zilla, get rid of cruft

--- lib/Document/TriPart.pm
+++ lib/Document/TriPart.pm
@@ -1,19 +1,9 @@
 package Document::TriPart;
+# ABSTRACT: Read, write & edit a tri-part document (preamble, YAML::Tiny header, and body)
+
 
 use warnings;
 use strict;
-
-=head1 NAME
-
-Document::TriPart - Read, write & edit a tri-part document (preamble, YAML::Tiny header, and body)
-
-=head1 VERSION
-
-Version 0.022
-
-=cut
-
-our $VERSION = '0.022';
 
 =head1 SYNOPSIS
 
@@ -352,61 +342,3 @@ sub edit {
 }
 
 1;
-
-=head1 AUTHOR
-
-Robert Krimen, C<< <rkrimen at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-document-tripart at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Document-TriPart>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Document::TriPart
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Document-TriPart>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Document-TriPart>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Document-TriPart>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Document-TriPart/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Robert Krimen, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of Document::TriPart
--- t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl
-
-use Test::More tests => 1;
-
-BEGIN {
-	use_ok( 'Document::TriPart' );
-}
-
-diag( "Testing Document::TriPart $Document::TriPart::VERSION, Perl $], $^X" );
